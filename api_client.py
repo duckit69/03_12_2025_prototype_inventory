9af796fcfe7ed38507db5118519e2ca76074797b
duckit69 <gherab.radouane@gmail.com>
2025-12-02 22:07:40 +0100

    updated db

--- api_client.py
+++ api_client.py
@@ -1,10 +1,10 @@
 # api_client.py - RFID to Article mapping
 
 rfid_to_article_mapping = {
-    "0386561177942": "Premium Coffee Beans",
-    "0210462015227": "Premium Coffee Beans",  # Same article, different tag
-    "0386561177942": "Organic Green Tea",
-    "0210462016561": "Organic Green Tea",     # Same article, different tag
+    "0210461153234": "Premium Coffee Beans",
+    "0386561177942": "Premium Coffee Beans",  # Same article, different tag
+    "0386561177952": "Organic Green Tea",
+    "0210462016561": "Ax3-0210462016561",     # Same article, different tag
     "0386561199531": "Hot Chocolategit",
     "0004859272": "Fresh Orange Juice",
     "0004859273": "Whole Wheat Bread",
